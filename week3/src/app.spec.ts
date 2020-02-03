6f9a0bdcb5b1f1d3aea45a1df144807e70968e7f
Ben Closson <closs290@regis.edu>
2020-02-02 22:35:06 -0700

    Ben Closson, 2020.02.02 MSSE 663, Week 3

--- week3/src/app.spec.ts
+++ week3/src/app.spec.ts
@@ -1,2 +1,23 @@
-import { expect } from 'chai';
 import 'mocha';
+import { expect } from 'chai';
+import { myCharacter, DNDStat } from './app';
+
+// var assert = require('assert');
+describe('MyCharacter', function() {
+    const exampleCharacter = new myCharacter('Example', 1, [DNDStat.Strength, DNDStat.Dexterity]);
+    describe('#isDown', function() {
+        it('should return true when HP is 0', function() {
+            exampleCharacter.CurrentHP = 0;
+            expect(exampleCharacter.isDown()).to.be(true);
+        });
+        it('should return false when HP is above 0', function() {
+            exampleCharacter.CurrentHP = 1;
+            expect(exampleCharacter.isDown()).to.be(false);
+        });
+    });
+    describe('#proficientInWhat', function() {
+        it('should return two stats', function() {
+            expect(exampleCharacter.proficientInWhat()).to.be([DNDStat.Strength, DNDStat.Dexterity]);
+        });
+    });
+});
--- week3/src/app.ts
+++ week3/src/app.ts
@@ -1,1 +1,56 @@
-// Source code here
+export enum DNDStat {
+    Strength,
+    Dexterity,
+    Constitution,
+    Intelligence,
+    Wisdom,
+    Charisma
+}
+
+export interface Character {
+    CharacterName: string;
+    HitPointTotal: number;
+    SavingThrows: DNDStat[];
+    isDown(): boolean;
+    proficientInWhat(): DNDStat[];
+}
+
+export class myCharacter implements Character {
+    CharacterName: string;
+    HitPointTotal: number;
+    CurrentHP: number;
+    SavingThrows: DNDStat[];
+
+    constructor(name: string, hp: number, saveThrows: DNDStat[]) {
+        this.CharacterName = name;
+        this.HitPointTotal = hp;
+        this.SavingThrows = saveThrows;
+    }
+    isDown(): boolean {
+        return (this.CurrentHP <= 0);
+    };
+    proficientInWhat(): DNDStat[] {
+        return this.SavingThrows;
+    };
+}
+
+const myName = 'Solis';
+let myHP = 100;
+let savingThrows = [DNDStat.Dexterity, DNDStat.Charisma];
+console.log("Creating a character with name " + myName + ", Hit Point Total: " + myHP + ", and ");
+console.log("Saving throws: " + savingThrows);
+const solis = new myCharacter(myName, myHP, savingThrows);
+
+console.log("\n Is he down? ");
+if (solis.isDown() ) {
+    console.log("yes")
+} else {
+    console.log("no")
+}
+
+let proficiencies = solis.proficientInWhat();
+console.log("He is proficient in the following saving throws: " + 
+    proficiencies.forEach(saveProficiency => {
+        saveProficiency;
+    })
+);
