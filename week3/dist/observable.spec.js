b281339b946afe07ee08893ba919885e7846ee26
Ben Closson <closs290@regis.edu>
2020-02-11 13:32:02 -0700

    This has been hanging out for a week

--- week3/dist/observable.spec.js
+++ week3/dist/observable.spec.js
@@ -5,13 +5,15 @@ require("mocha");
 const observable_1 = require("./observable");
 describe('observableAssignment', function () {
     const newObservable = new observable_1.observableAssignment();
+    // make observable and array?
     it('should create an observable', function () {
-        chai_1.expect(this.newObservable.observeStreamOfArray).to.be.a('object');
+        chai_1.expect(newObservable.observeStreamOfArray).to.be.an('object');
     });
     it('should create a map of the values in the array', function () {
-        chai_1.expect(this.newObservable.myArray).to.equal([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+        chai_1.expect(newObservable.myArray).to.include(0);
+        chai_1.expect(newObservable.myArray).to.include(9);
     });
     it('should subscribe to the observable', function () {
-        this.newObservable.observeStreamOfArray.subscribe((result) => chai_1.expect(result.length).to.be.greaterThan(0));
+        newObservable.observeStreamOfArray.subscribe((result) => chai_1.expect(result.length).to.be.greaterThan(0));
     });
 });
