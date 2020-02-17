e19e9ed1a6249cba9291c3ef09d82bde605000f5
closs290 <59987065+closs290@users.noreply.github.com>
2020-02-16 20:51:47 -0700

    Slightly better tests.

--- week5/src/object-destructuring.spec.ts
+++ week5/src/object-destructuring.spec.ts
@@ -1,5 +1,15 @@
 import { expect } from 'chai';
 import 'mocha';
+import {
+  object,
+  nestedObject,
+  user,
+  address,
+  id,
+  newObject,
+  greeting,
+  statement
+} from './object-destructuring';
 
 describe('Object destructuring tests', () => {
   it('#1 should have a variable for every property in the given object', () => {
@@ -16,8 +26,10 @@ describe('Object destructuring tests', () => {
   });
 
   it('#3 should create a new object given the destructed variables from the nested object', () => {
-    // expect newObject to be an object
-    // expect user to be a string
+    expect(newObject).to.be.an('object');
+    expect(user).to.equal(nestedObject.user); // maybe this should be #2, and #3 is testing something different.
+    expect(address).to.equal(nestedObject.address); // objects are hard to check
+    expect(id).to.equal(nestedObject.id);
     // expect newObject to contain user
     // expect newObject to contain nested city
     // expect newObject to contain nested state
@@ -26,6 +38,6 @@ describe('Object destructuring tests', () => {
   });
 
   it('#4 should construct a statement from the given object', () => {
-    // expect statement to equal 'hello, taylor!'
+     expect(statement).to.be.an('object');
   });
 });
