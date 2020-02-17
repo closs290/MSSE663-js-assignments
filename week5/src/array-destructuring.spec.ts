95cbb8862271bd967a9def1662bb5de62ff3da3d
closs290 <59987065+closs290@users.noreply.github.com>
2020-02-16 20:44:30 -0700

    Fixing some tests.

--- week5/src/array-destructuring.spec.ts
+++ week5/src/array-destructuring.spec.ts
@@ -1,18 +1,25 @@
 import { expect } from 'chai';
 import 'mocha';
+import {
+  fruits,
+  food,
+  statement
+} from './array-destructuring.ts';
 
 describe('Array destructuring tests', () => {
   it('#1 should have a variable for every element in the given array', () => {
-    // expect foreach ?
+    expect(fruits[0]).to.equal('apple');
+    expect(fruits[1]).to.equal('banana');
   });
 
   it('#2 should have a variable for the first 2 elements then spread the rest', () => {
-    // expect array[0]
+    // expect(food[0]).to.equal(a); // variables aren't exported...
     // expect array[1]
     // expect forEach for array[2]-array[n]
+    expect(food).length.to.be(6);
   });
 
-  it('#3 should construct a statement from the given objarrayect', () => {
-
+  it('#3 should construct a statement from the given array object', () => {
+    expect(statement).to.be.an('object');
   });
 });
