e1639f3c00edf10a9bbfd7735ccc584b4077019f
closs290 <59987065+closs290@users.noreply.github.com>
2020-03-05 11:42:47 -0800

    These tests should pass

--- week5/src/object-destructuring.spec.ts
+++ week5/src/object-destructuring.spec.ts
@@ -1,43 +1,33 @@
 import { expect } from 'chai';
 import 'mocha';
 import {
-  object,
+  person,
+  job,
   nestedObject,
   user,
   address,
   id,
   newObject,
-  greeting,
   statement
 } from './object-destructuring';
 
 describe('Object destructuring tests', () => {
   it('#1 should have a variable for every property in the given object', () => {
-    // expect(person).to.exist();
-    // expect(job).to.exist();
-    // expect length of variables to equal length of object properties
+    expect(person).to.eql('elvis');
+    expect(job).to.eql('hip swinger');
   });
 
   it('#2 should have a variable for every property in the given nested object', () => {
-    // expect user to exist;
-    // expect address to exist;
-    // expect address to be an object;
-    // expect id to exist;
+    expect(user).to.eql('elvis');
+    expect(address).to.eql({ city: 'denver', state: 'colorado' });
+    expect(id).to.eql(1);
   });
 
   it('#3 should create a new object given the destructed variables from the nested object', () => {
-    expect(newObject).to.be.an('object');
-    expect(user).to.equal(nestedObject.user); // maybe this should be #2, and #3 is testing something different.
-    expect(address).to.equal(nestedObject.address); // objects are hard to check
-    expect(id).to.equal(nestedObject.id);
-    // expect newObject to contain user
-    // expect newObject to contain nested city
-    // expect newObject to contain nested state
-    // expect newObject to contain id
-    // expect id to be a number
+    expect(newObject).to.be.eql(nestedObject);
   });
 
   it('#4 should construct a statement from the given object', () => {
-     expect(statement).to.be.an('object');
+     expect(statement).to.eql('hello, taylor');
   });
 });
