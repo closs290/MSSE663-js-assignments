0d5843cdef96546b468728293f26b94287ffb61b
closs290 <59987065+closs290@users.noreply.github.com>
2020-03-05 11:41:52 -0800

    Fixing to actually use object destructuring

--- week5/src/object-destructuring.ts
+++ week5/src/object-destructuring.ts
@@ -7,12 +7,7 @@ const job = object.title;
 
 // Assign variables using ES6 so that we get (note, you cannot just make a completely new object):
 console.log(person); // 'elvis'
-console.log(job); // 'hip swinger'
-
-// #2 Object Matching: Nested destructuring
-export const nestedObject = {
-  user: 'elvis',
-  address: {
+console.log(job); // 'hd
     city: 'denver',
     state: 'colorado'
   },
@@ -20,9 +15,7 @@ export const nestedObject = {
 };
 
 // deconstruct here
-export const user = nestedObject.user;
-export const address = nestedObject.address;
-export const id = nestedObject.id;
+export const {user, address, id} = nestedObject;
 
 // expected results:
 console.log(user); // 'elvis'
@@ -32,16 +25,15 @@ console.log(id); // 1
 // #3 Create a new object given the destructed values above
 
 // reconstruct here
-
 export const newObject = {user, ...address, id};
 
 // #4 Object with key value pairs: construct a statement
 
 // Not sure if this is useful: var { foo, bar } = { foo: "lorem", bar: "ipsum" };
 
-export const greeting = { greeting: 'hello', name: 'taylor' };
+export const {greeting, name} = { greeting: 'hello', name: 'taylor' };
 
 // construct statement here
-export const statement = console.log(greeting.greeting + ', ' + greeting.name + '!');
+export const statement = console.log(greeting + ', ' + name + '!');
 
 // expected result: 'hello, taylor!'
